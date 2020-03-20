8b385e14a805e0d41dc32d7c62737931a51f9675
Swathi N Shayana <55015685+Swathi-N-Shayana@users.noreply.github.com>
2020-03-20 20:32:54 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,9 @@
+#Task 
+#Read two integers and print two lines. The first line should contain integer division,  a//b . The second line should contain float #division,  a/b .
+
+#You don't need to perform any rounding or formatting operations.
+from __future__ import division
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
